c2807d865db34694405fb25ac91cb8b682005258
Viktor-sun <bobick.zdoh@gmail.com>
2021-11-30 15:57:58 +0200

    fix: fix conditions

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
 const http = require("http");
+const url = require("url");
 
 let todos = [
   { id: 1, todo: "asd" },
-  { id: 2, todo: "asd" },
+  { id: 2, todo: "asdas" },
 ];
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
 
-  switch (req.url) {
-    case "/":
-      res.writeHead(200);
+  const todoId = Number(url.parse(req.url, true).query.todoId);
+
+  if (req.url === "/todos" && req.method === "GET") {
+    res.writeHead(200);
+    res.end(
+      JSON.stringify({
+        status: "success",
+        code: 200,
+        todos: todos,
+      })
+    );
+  } else if (req.url === "/todos" && req.method === "POST") {
+    req.on("data", (data) => {
+      res.writeHead(201);
+      todos.push(JSON.parse(data));
 
       res.end(
         JSON.stringify({
           status: "success",
-          code: 200,
-          todos: todos,
+          code: 201,
+          message: "todo added",
+          todos,
         })
       );
-      break;
+    });
+  } else if (todoId && req.method === "DELETE") {
+    const hasTodo = todos.find((todo) => todo.id === todoId);
 
-    case "/todos/add":
+    if (hasTodo) {
+      res.writeHead(204, "todo deleted");
+      todos = todos.filter((todo) => todo.id !== todoId);
+      res.end();
+    } else {
+      res.writeHead(404);
+      res.end(
+        JSON.stringify({
+          status: "error",
+          code: 404,
+          message: "todo not found",
+        })
+      );
+    }
+  } else if (todoId && req.method === "PATCH") {
+    const hasTodo = todos.find((todo) => todo.id === todoId);
+
+    if (hasTodo) {
       req.on("data", (data) => {
-        res.writeHead(201);
-        todos.push(JSON.parse(data));
-
-        res.end(
-          JSON.stringify({
-            status: "success",
-            code: 201,
-            message: "todo added",
-            todos,
-          })
-        );
-      });
-      break;
-
-    case "/todos/delete":
-      req.on("data", (todoId) => {
         res.writeHead(200);
-
-        const parsedId = JSON.parse(todoId);
-
-        // const hasTodo = todos.find((todo) => todo.id === parsedId.id);
-
-        todos = todos.filter((todo) => todo.id !== parsedId.id);
-
-        res.end(
-          JSON.stringify({
-            status: "success",
-            code: 200,
-            message: "todo deleted",
-            todos,
-          })
-        );
-      });
-      break;
-
-    case "/todos/update":
-      res.writeHead(200);
-      req.on("data", (data) => {
-        const parsedData = JSON.parse(data);
+        const { updatedTodo } = JSON.parse(data);
 
         todos = todos.map((todo) => {
-          if (parsedData.id === todo.id) {
-            todo.todo = parsedData.updatedTodo;
+          if (todoId === todo.id) {
+            todo.todo = updatedTodo;
           }
           return todo;
         });
@@ -79,15 +75,32 @@ const server = http.createServer((req, res) => {
           })
         );
       });
-      break;
-
-    default:
+    } else {
       res.writeHead(404);
-      res.end(JSON.stringify({ error: "Resource not found" }));
+      res.end(
+        JSON.stringify({
+          status: "error",
+          code: 404,
+          message: "todo not found",
+        })
+      );
+    }
+  } else if (!todoId) {
+    res.writeHead(400);
+    res.end(
+      JSON.stringify({
+        status: "error",
+        code: 400,
+        message: "bad request",
+      })
+    );
+  } else {
+    res.writeHead(404);
+    res.end(JSON.stringify({ error: "Resource not found" }));
   }
 });
 
-const PORT = 8080;
+const PORT = 8050;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
